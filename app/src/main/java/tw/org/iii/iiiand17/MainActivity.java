3164239e80feee2fc4f68dcc5c83c1dfe724f6f6
Annie <https://github.com/yujan1122/practice1.git>
2019-12-10 16:20:52 +0800

    https://freesound.org/people/ljudman/sounds/33245/
    音樂播放

--- app/src/main/java/tw/org/iii/iiiand17/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiand17/MainActivity.java
@@ -6,12 +6,18 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private MyReciver receiver;
+    private SeekBar seekBar;//當進度條
 //前景處理
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +42,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init(){
+        seekBar = findViewById(R.id.seekBar);
+        receiver = new MyReciver();
+        //拉動seekbar,互動
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    Intent intent = new Intent(MainActivity.this, MyService.class);
+                    intent.putExtra("cmd","seekto");//傳遞前景至MyService處理
+                    intent.putExtra("nowpos",progress);//傳遞前景至MyService處理
+                    startService(intent);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
     }
 
     //背景service
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(receiver, new IntentFilter());
+    }
 
     public void play(View view) {
         Intent intent = new Intent(this, MyService.class);
@@ -58,5 +96,19 @@ public class MainActivity extends AppCompatActivity {
     public void reset(View view) {
         Intent intent = new Intent(this, MyService.class);
         stopService(intent);
+    }
+
+    //廣播接收器
+    private class MyReciver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int pos = intent.getIntExtra("now",1);
+            int len = intent.getIntExtra("len",1);
+            if(len>=0){
+                seekBar.setMax(len);
+            }else if(pos>=0){
+                seekBar.setMax(pos);
+            }
+        }
     }
 }
--- app/src/main/java/tw/org/iii/iiiand17/MyService.java
+++ app/src/main/java/tw/org/iii/iiiand17/MyService.java
@@ -9,10 +9,15 @@ import android.os.IBinder;
 import android.util.Log;
 
 import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+
 //播放音樂檔處理
 public class MyService extends Service {
     private MediaPlayer mediaPlayer;
     private File sdroot;
+    private Timer timer;//時間週期
+
 
     //綁定型服務, 必須override; 但我們這邊不做綁定型的
     @Override
@@ -22,15 +27,29 @@ public class MyService extends Service {
         return null;
     }
 
+    private class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                int pos = mediaPlayer.getCurrentPosition();
+                sendBroadcast(new Intent("PLAY_NOW").putExtra("now",pos));//背景中 0.1s廣播發送
+                //已經沒有他的事, 回到MainActivity怎麼收
+            }
+        }
+    }
+
     //啟動後 onCreate只會啟動一次
     @Override
     public void onCreate() {
         super.onCreate();
         try{
+            timer = new Timer();
+            timer.schedule(new MyTask(),0,100);//0.1秒回應哥撥放到哪裡
             sdroot = Environment.getDataDirectory();//取得路徑
             //https://developer.android.com/reference/android/media/MediaPlayer
             mediaPlayer = new MediaPlayer();
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            Log.v("brad","sdcard"+ sdroot.getAbsolutePath());
             mediaPlayer.setDataSource(sdroot.getAbsolutePath() + "/s1.mp3");
             mediaPlayer.prepare();
         }catch (Exception e){
@@ -49,6 +68,10 @@ public class MyService extends Service {
             mediaPlayer.pause();
         }else if(!mediaPlayer.isPlaying() && (cmd.equals("play"))) {
             mediaPlayer.start();
+            sendBroadcast(new Intent("PLAY_NOW").putExtra("len",mediaPlayer.getDuration()));
+        }else if(cmd.equals("seekto")){
+            int nowpos = intent.getIntExtra("nowpos",-1);
+            if(nowpos >= 0) mediaPlayer.seekTo(nowpos);
         }
         return super.onStartCommand(intent, flags, startId);
     }
@@ -59,6 +82,11 @@ public class MyService extends Service {
         if(mediaPlayer!=null){
             if(mediaPlayer.isPlaying()) mediaPlayer.stop();
             mediaPlayer.release();
+        }
+        if(timer != null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
         }
 //        Log.v("brad", "onDestroy");
     }
